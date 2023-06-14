d93507dcb7332a8032aa89092c6dca6642f9f9cd
VuranokYigit <fyvuranokk@gmail.com>
2023-06-14 23:29:26 +0300

    profTestCreate

--- src/Navbar/Quizzes/profTest.jsx
+++ src/Navbar/Quizzes/profTest.jsx
@@ -1,120 +1,95 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import './profTest.css';
 import Questions from "../questionPool.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faForward } from '@fortawesome/free-solid-svg-icons';
 
 const Quizzes = (props) => {
-    const easyQ = [];    // easy (kolay) zorluk seviyesine ait soruların dizisi
-    const mediumQ = [];  // medium (orta) zorluk seviyesine ait soruların dizisi
-    const hardQ = [];    // hard (zor) zorluk seviyesine ait soruların dizisi
-
-    // JSON dosyasındaki tüm soruları dolaşarak zorluk seviyesine göre ayırma
-    for (let i = 0; i < Questions.questions.length; i++) {
-        const question = Questions.questions[i];
-        if (question.difficulty === "easy") {
-            easyQ.push(question);
-        } else if (question.difficulty === "medium") {
-            mediumQ.push(question);
-        } else if (question.difficulty === "hard") {
-            hardQ.push(question);
-        }
-    }
-    const wrongAnswers = {
-        easy: [],
-        medium: [],
-        hard: []
-    };
-
-    // Kullanıcının verdiği cevaplara göre yanlış cevapları kaydetme
-    function saveWrongAnswers(question, userAnswer) {
-        wrongAnswers[question.difficulty].push({
-            question: question,
-            userAnswer: userAnswer
-        });
-    }
-    const selectedQuestions = [];  // Seçilen soruların dizisi
-    const maxQuestionsPerDifficulty = 2;  // Her zorluk seviyesinden kaç soru seçileceği
-
-    // Easy (kolay) zorluk seviyesinden 2 soru seçme
-    for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-        const randomIndex = Math.floor(Math.random() * easyQ.length);
-        selectedQuestions.push(easyQ[randomIndex]);
-        easyQ.splice(randomIndex, 1);  // Seçilen soruyu easyQ dizisinden kaldırma
-    }
-
-    // Medium (orta) zorluk seviyesinden 2 soru seçme
-    for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-        const randomIndex = Math.floor(Math.random() * mediumQ.length);
-        selectedQuestions.push(mediumQ[randomIndex]);
-        mediumQ.splice(randomIndex, 1);  // Seçilen soruyu mediumQ dizisinden kaldırma
-    }
-
-    // Hard (zor) zorluk seviyesinden 2 soru seçme
-    for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-        const randomIndex = Math.floor(Math.random() * hardQ.length);
-        selectedQuestions.push(hardQ[randomIndex]);
-        hardQ.splice(randomIndex, 1);  // Seçilen soruyu hardQ dizisinden kaldırma
-    }
+ 
+    const [easyQuestions , setEasyQuestions] = useState(Questions.questions.filter(x=>x.difficulty == "easy"))
+    const [mediumQuestions , setMediumQuestions] = useState(Questions.questions.filter(x=>x.difficulty == "medium"))
+    const [hardQuestions , setHardQuestions] = useState(Questions.questions.filter(x=>x.difficulty == "hard"))
+    const [selectedQuestions , setSelectedQuestions] = useState([])
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState();
+    const [userAnswerText,setUserAnswerText] = useState("");
+    const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+    const [correctAnswers, setCorrectAnswers] = useState({});
     const [wrongAnswersCount, setWrongAnswersCount] = useState(0);
+    const [wrongAnswers, setWrongAnswers] = useState({ });
+    const [pageLoaded, setPageLoaded] = useState(false)
 
-    const handleNextQuestion = () => {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-
-    };
-
-    var currentQuestion = selectedQuestions[currentQuestionIndex];
-    const handleAnswer = (selectedAnswer) => {
-        
-        const isAnswerCorrect = currentQuestion.answer1 === selectedAnswer || currentQuestion.answer2 === selectedAnswer;
-        console.log("calisti");
-        if (isAnswerCorrect) {
-            setCorrectAnswers(correctAnswers + 1);
-        } else {
-            setWrongAnswersCount(wrongAnswersCount + 1);
-            saveWrongAnswers(currentQuestion, selectedAnswer);
+  
+    useEffect(()=> {
+        let questionArray = [];
+        for (let i = 0; i < 2; i++) {
+            const randomIndexe = Math.floor(Math.random() * easyQuestions.length);
+            
+            questionArray = [...questionArray , easyQuestions[randomIndexe]]
+            easyQuestions.splice(randomIndexe, 1); 
         }
+        
+        for (let j = 0; j < 2; j++) {
+            const randomIndexx = Math.floor(Math.random() * mediumQuestions.length);
+            questionArray = [...questionArray , mediumQuestions[randomIndexx]]
+            mediumQuestions.splice(randomIndexx, 1);  
+        }
+        for (let r = 0; r < 2; r++) {
+            const randomIndexz = Math.floor(Math.random() * hardQuestions.length);
+            questionArray = [...questionArray , hardQuestions[randomIndexz]]
+            hardQuestions.splice(randomIndexz, 1); 
+        }
+        setSelectedQuestions(questionArray)
+        setCurrentQuestion(questionArray[currentQuestionIndex].question)
+    },[])
 
-        handleNextQuestion();
-    };
 
+  
+    const handleNextQuestion = () => {
+        if (userAnswerText == selectedQuestions[currentQuestionIndex].answer1 || userAnswerText == selectedQuestions[currentQuestionIndex].answer2){
+            setCorrectAnswersCount(correctAnswersCount + 1)
+            setCorrectAnswers({...correctAnswers, [selectedQuestions[currentQuestionIndex].id]: userAnswerText})
+        }else{
+            setWrongAnswersCount(wrongAnswersCount + 1)
+            setWrongAnswers({...wrongAnswers, [selectedQuestions[currentQuestionIndex].id]: userAnswerText})
+        }
+        
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setUserAnswerText("");
+        if(currentQuestionIndex + 1 <= 5){
+            setCurrentQuestion(selectedQuestions[currentQuestionIndex+1].question);
+        }
+        
+        
+      };
+      
 
-    // "Try Again" butonuna tıklama olayını yakalama
+    //"Try Again" butonuna tıklama olayını yakalama //bu kismi ayaga kaldir//
     function handleTryAgain() {
         // Seçilen soruları sıfırlama
-        selectedQuestions.length = 0;
-
-        // Yanlış cevapları tutan dizileri sıfırlama
-        wrongAnswers.easy.length = 0;
-        wrongAnswers.medium.length = 0;
-        wrongAnswers.hard.length = 0;
-
-        // Yeni soruları seçme işlemini tekrar yapma
-
-        // Easy (kolay) zorluk seviyesinden 2 soru seçme
-        for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-            const randomIndex = Math.floor(Math.random() * easyQ.length);
-            selectedQuestions.push(easyQ[randomIndex]);
-            easyQ.splice(randomIndex, 1);
+        setSelectedQuestions([])
+        setCurrentQuestionIndex(0)
+        for (let i = 0; i < 2; i++) {
+            const randomIndex = Math.floor(Math.random() * easyQuestions.length);
+            setSelectedQuestions([...selectedQuestions,easyQuestions[randomIndex]]) 
+            easyQuestions.splice(randomIndex, 1); 
         }
-
-        // Medium (orta) zorluk seviyesinden 2 soru seçme
-        for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-            const randomIndex = Math.floor(Math.random() * mediumQ.length);
-            selectedQuestions.push(mediumQ[randomIndex]);
-            mediumQ.splice(randomIndex, 1);
+        for (let i = 0; i < 2; i++) {
+            const randomIndex = Math.floor(Math.random() * mediumQuestions.length);
+            setSelectedQuestions([...selectedQuestions,mediumQuestions[randomIndex]]);
+            mediumQuestions.splice(randomIndex, 1);  
         }
-
-        // Hard (zor) zorluk seviyesinden 2 soru seçme
-        for (let i = 0; i < maxQuestionsPerDifficulty; i++) {
-            const randomIndex = Math.floor(Math.random() * hardQ.length);
-            selectedQuestions.push(hardQ[randomIndex]);
-            hardQ.splice(randomIndex, 1);
+        for (let i = 0; i < 2; i++) {
+            const randomIndex = Math.floor(Math.random() * hardQuestions.length);
+            setSelectedQuestions([...selectedQuestions,hardQuestions[randomIndex]]);
+            hardQuestions.splice(randomIndex, 1); 
         }
     }
-    //console.log(hardQ.length);
+  
+    const handleUserAnswerText = (event) => {
+        setUserAnswerText(event.target.value)
+ 
+    }
     return (
         <div className="offcanvas-header">
             <form id='manipulatinSpanTagsLoginForm' action="#" method='get' autoComplete='on'>
@@ -129,21 +104,19 @@ const Quizzes = (props) => {
                                     data-bs-dismiss="offcanvas"
                                     aria-label="Close"
                                     onClick={props.handleClose}
-                                ><FontAwesomeIcon id='closeIcon' icon={faXmark} style={{ color: "#03e9f4", }} /></button>
-                                <h2>TEST</h2>
-                                <form>
-                                <p id="currentQuestion">{currentQuestion.question}</p>
-                                    <div className="user-box">
-                                        <input type="text" name="" required="" placeholder="Your Answer" />
-                                        <div>
-                                            
-                                           
-                                        </div>
-                                    </div>
-                                    <a href="#">
-                                        Next <FontAwesomeIcon icon={faForward} onClick={handleNextQuestion} />
-                                    </a>
-                                </form>
+                                >
+                                    <FontAwesomeIcon id='closeIcon' icon={faXmark} style={{ color: "#03e9f4", }} /></button>
+
+                                {currentQuestionIndex <= 5 ? (
+                                    <TestView currentQuestion={currentQuestion} handleNextQuestion={handleNextQuestion} setUserAnswerText={setUserAnswerText} handleUserAnswerText={handleUserAnswerText} userAnswerText={userAnswerText}  selectedQuestions={selectedQuestions}/>
+                                ) : (
+                                    <FinishQuiz
+                                        selectedQuestions={selectedQuestions}
+                                        handleTryAgain={handleTryAgain}
+                                        correctAnswersCount={correctAnswersCount}
+                                        wrongAnswersCount={wrongAnswersCount}
+                                    />
+                                )}
                             </div>
                         </>
                     </div>
@@ -153,5 +126,54 @@ const Quizzes = (props) => {
         </div>
     )
 }
+
+const TestView = (props) => {
+    const { currentQuestion, handleNextQuestion, handleAnswer,setUserAnswerText,handleUserAnswerText,userAnswerText ,selectedQuestions } = props
+    
+    return (
+        <>
+            <h2>TEST</h2>
+            <br /><br />
+            <form>
+                <p id="currentQuestion">{currentQuestion}</p>
+                <div className="user-box">
+                    <input
+                        type="text"
+                        value={userAnswerText}
+                        name="userAnswer"
+                        required=""
+                        placeholder="Your Answer"
+                        onChange={(event) => handleUserAnswerText(event)}
+                    />
+
+                </div>
+
+                <a href="#">
+                    Next <FontAwesomeIcon icon={faForward} onClick={handleNextQuestion} />
+                </a>
+
+            </form>
+        </>
+    )
+}
+
+
+const FinishQuiz = (props) => {
+    const { selectedQuestions, handleTryAgain, correctAnswersCount, wrongAnswersCount } = props
+    return (
+        <>
+            <h2>TEST RESULTS</h2>
+            <form>
+                <p>Questions: {selectedQuestions.length}</p>
+                <p>Correct Answers: {correctAnswersCount}</p>
+                <p>Wrong Answers: {wrongAnswersCount}</p>
+                <a href="#">
+                    Try Again <FontAwesomeIcon icon={faForward} onClick={handleTryAgain} />
+                </a>
+            </form>
+        </>
+
+    );
+};
 
 export default Quizzes;
